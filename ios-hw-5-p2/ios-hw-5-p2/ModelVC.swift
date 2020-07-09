e3d1a09bb1e11da804f76bea7b80367a2be47652
rawaanalshayji <66734178+rawaanalshayji@users.noreply.github.com>
2020-07-10 02:20:09 +0300

    done
    done

---
+++ ios-hw-5-p2/ios-hw-5-p2/ModelVC.swift
@@ -0,0 +1,30 @@
+//
+//  ModelVC.swift
+//  ios-hw-5-p2
+//
+//  Created by Rawaan Alshayji on 7/9/20.
+//  Copyright © 2020 Rawaan Alshayji. All rights reserved.
+//
+
+import UIKit
+
+class ModelVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
